64183ffb7883a67b5a45cfe2a2141d64ffa48434
marceloribeirosilva <ribmarc@gmail.com>
2020-08-04 15:22:03 -0300

    Excluindo horários antigos

--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate, getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -43,14 +43,20 @@ class ListProviderDayAvailabilityService {
       (_, index) => index + hourStart,
     );
 
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
-      const hasAppointmentInHour = appointments.find(app => getHours(app.date) === hour);
+      const hasAppointmentInHour = appointments.find(
+        app => getHours(app.date) === hour,
+      );
+
+      const compareDate = new Date(year, month - 1, day, hour);
 
       return {
         hour,
-        available: !hasAppointmentInHour,
-      }
-    })
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
+      };
+    });
 
     return availability;
   }
