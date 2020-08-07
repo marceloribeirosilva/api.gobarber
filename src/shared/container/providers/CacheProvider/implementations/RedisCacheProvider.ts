8cbecee4a1dbbc4a0f86d834d9000c55553199e8
marceloribeirosilva <ribmarc@gmail.com>
2020-08-07 08:30:41 -0300

    Invalidando cache

--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -9,12 +9,25 @@ export default class RedisCacheProvider implements ICacheProvider {
     this.client = new Redis(cacheConfig.config.redis);
   }
 
+
   public async save(key: string, value: any): Promise<void> {
     this.client.set(key, JSON.stringify(value));
   }
 
   public async invalidate(key: string): Promise<void> {
     throw new Error("Method not implemented.");
+  }
+
+  public async invalidatePrefix(prefix: string): Promise<void> {
+    const keys = await this.client.keys(`${prefix}:*`);
+
+    const pipeline = this.client.pipeline();
+
+    keys.forEach(key => {
+      pipeline.del(key);
+    });
+
+    await pipeline.exec();
   }
 
   public async recover<T>(key: string): Promise<T | null> {
